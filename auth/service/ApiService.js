9a906628a5b4cc56bffc0fd7c7a02470873fedd1
Antun Delinger <antun994@gmail.com>
2022-07-29 20:37:20 +0200

    fix responsivnes problem

--- auth/service/ApiService.js
+++ auth/service/ApiService.js
@@ -4,8 +4,8 @@ export default class ApiService {
   constructor() {
     this.api_token = null;
     this.client = null;
-     this.api_url = "https://localhost:5001/api/ateliercars";
-    //this.api_url = process.env.NEXT_PUBLIC_URL;
+     //this.api_url = "https://localhost:5001/api/ateliercars";
+    this.api_url = process.env.NEXT_PUBLIC_URL;
   }
   init = () => {
     // this.api_token = getCookie("ACCESS_TOKEN");

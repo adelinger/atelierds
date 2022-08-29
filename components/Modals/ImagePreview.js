0f108cc8858f3cc7f3ecdb69bd625a064f8f0f7d
Antun Delinger <antun994@gmail.com>
2022-08-29 19:10:36 +0200

    close on esc

--- components/Modals/ImagePreview.js
+++ components/Modals/ImagePreview.js
@@ -1,14 +1,15 @@
 import React, { useRef } from 'react';
+import { useDetectClickOutside } from 'react-detect-click-outside';
 
 function ImagePreview({showModal, src, toggleModal}) {
-    const modalRef = useRef(null);
     const imageTagRef = useRef(null);
 
+    const ref = useDetectClickOutside({ onTriggered: toggleModal });
 
     return (
     <>
     {showModal &&
-    <div id="modal" ref={modalRef}
+    <div id="modal" ref={ref}
     class="fixed top-0 left-0 w-screen h-screen bg-black/70 flex justify-center items-center">
 
     <a class="fixed z-90 top-6 right-8 text-white text-5xl font-bold" href="javascript:void(0)" onClick={toggleModal}

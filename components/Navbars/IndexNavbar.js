91b11ca5455e2150f34de585d30f5f8aaa27c9e0
Ivona Crnković <crnkovicivona@gmail.com>
2022-09-06 18:13:43 +0200

    facebook link added, twitter icon deleted

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -121,11 +121,11 @@ export default function Navbar(props) {
               <li className="flex items-center">
                 <a
                   className="lg:text-white lg:hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href=""
+                  href="https://www.facebook.com/Atelier-DS-2049109098648782/"
                   target="_blank"
                 >
                   <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg " />
-                  <span className="lg:hidden inline-block ml-2">Check our Facebook</span>
+                  <span className="lg:hidden inline-block ml-2" >Check our Facebook</span>
                 </a>
               </li>
 

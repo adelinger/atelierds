77e30213b405665bfc5717fa2d22ae31e7fbbba3
Ivona Crnković <crnkovicivona@gmail.com>
2022-09-09 10:05:22 +0200

    price_on_enquiry added, translated

--- components/Cards/ItemListCard.js
+++ components/Cards/ItemListCard.js
@@ -8,7 +8,7 @@ import { height } from "tailwindcss/defaultTheme";
 
 export default function ItemListCard({ car, serverUrl }) {
 
-  const { t } = useTranslation('index');
+  const { t } = useTranslation('index', 'price');
 
   return (
     <>
@@ -39,7 +39,9 @@ export default function ItemListCard({ car, serverUrl }) {
                 {t('carsPage:read_more')}
                 <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </Link>
-              <p className="float-right text-3x1 font-bold mt-7">{car.carPrice}€</p>
+              <p className="float-right text-3x1 font-bold mt-7">
+               {car.carPrice != 0 ? car.carPrice +'€': t('common:price_on_enquiry')}
+                </p>
             </div>
 
 

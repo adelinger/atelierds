6b3ef2919eb1e2a5e5ffdb4b1cc3a3ad2f6ad688
Ivona Crnković <crnkovicivona@gmail.com>
2022-09-22 08:55:12 +0200

    navbar and logo edit

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function Error404 () {
 const handleOnClick = () => {
   Router.push('/')
 }
-
+const { t } = useTranslation('chromePage');
 return (
       <>
  <div class="flex items-center justify-center w-screen h-screen">

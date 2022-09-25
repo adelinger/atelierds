b78a9699b808e35e73a944b7aef4213f6e50d213
Antun Delinger <antun994@gmail.com>
2022-09-25 21:06:53 +0200

    car page

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -13,29 +13,29 @@ import Image from "next/image";
 
 export default function carsForSale({ cars, serverUrl }) {
   const { t } = useTranslation('carsPage');
-  
+
   return (
     <>
       <Navbar />
       <main>
-        <div className="relative pt-16 pb-16 md:pb-32 flex content-center items-center justify-center min-h-screen-75">
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
           <div
             className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
               backgroundImage:
-                "url('/img/light-grey-19.webp')",
+                "url('/img/image_ds.webp')",
             }}
           >
             <span
               id="blackOverlay"
-              className="w-full h-full absolute opacity-50 bg-black"
+              className="w-full h-full absolute opacity-75 bg-black"
             ></span>
           </div>
           <div className="container relative mx-auto">
             <div className="items-center flex flex-wrap">
-              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center mt-5">
-                <div className="">
-                  <h1 className="text-white font-semibold text-5xl mt-5">
+              <div className="w-full lg:w-6/12 mt-5 md:mt-0 md:px-4 ml-auto mr-auto text-center">
+                <div>
+                <h1 className="text-white font-semibold text-5xl mt-5">
                     {t('cars_for_sale')}
                   </h1>
                   <p className="mt-4 text-lg text-blueGray-200">
@@ -48,35 +48,58 @@ export default function carsForSale({ cars, serverUrl }) {
                   </p>
                 </div>
               </div>
-              <div className="container mx-auto ">
+            </div>
+          </div>
+          <div
+            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16"
+            style={{ transform: "translateZ(0)" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                className="text-blueGray-800 fill-current"
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+        </div>
+
+        <section className="pb-20 bg-blueGray-800 -mt-72">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-8 rounded-lg">
                 {
-                  cars.length === 0 ? 
-                 <div className="text-center">
-                    <Image
-                    alt="No results" 
-                    src="/img/errors/no-results.webp"
-                    width={300}
-                    height={300}
-                    ></Image>
-                 </div>
+                  cars.length === 0 ?
+                    <div className="text-center">
+                      <Image
+                        alt="No results"
+                        src="/img/errors/no-results.webp"
+                        width={300}
+                        height={300}
+                      ></Image>
+                    </div>
                     :
-                    <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-3">
+                    <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-3 flex-auto">
                       {cars.map(car =>
                         <div className="mt-5 mb-5 ml-auto mr-auto" >
                           <CarCard car={car} serverUrl={serverUrl}></CarCard>
                         </div>
                       )}
-
+  
                     </div>
                 }
-
-
-              </div>
+             </div>
 
             </div>
           </div>
-
-        </div>
+        </section>
 
       </main>
       <Footer />

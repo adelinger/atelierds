71fbeac64cdeffeb2119f0953bb99f2130bb9551
Ivona Crnković <crnkovicivona@gmail.com>
2022-09-26 13:06:15 +0200

    bug fix

--- components/Cards/CarCard.js
+++ components/Cards/CarCard.js
@@ -51,7 +51,7 @@ export default function CarCard({ car, serverUrl }) {
                 {t('carsPage:read_more')}
                 <svg class="ml-2 mt-1 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </Link>
-              <p className="float-right text-3x1 font-bold mt-5 text-blueGray-400">
+              <p className="float-right text-3x1 font-bold mt-5 text-blueGray-400 w-28" >
                {car.carPrice != 0 ? numberWithCommas(car.carPrice)+ ',00' +' €': t('common:price_on_enquiry')}
                 </p>
             </div>

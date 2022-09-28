0fc20341e6fc7a31a3b00874f6c7d78ea785a6ae
Ivona Crnković <crnkovicivona@gmail.com>
2022-09-28 21:16:47 +0200

    show more visibility

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -22,13 +22,13 @@ export default function carsForSale({ cars, serverUrl }) {
 
   const getAllCars = async (e) => {
     api
-    .getCarsPublic()
-    .then(response => response.data)
-    .then(data => {
-      setCarsList(data);
-    })
-    .catch((error) => {
-    });
+      .getCarsPublic()
+      .then(response => response.data)
+      .then(data => {
+        setCarsList(data);
+      })
+      .catch((error) => {
+      });
   }
 
   const toggleDropdown = () => {
@@ -66,48 +66,48 @@ export default function carsForSale({ cars, serverUrl }) {
                       t('title_message')
                     }
                   </p>
-                    <div>
-                      <button id="dropdownRadioBgHoverButton" onClick={toggleDropdown} data-dropdown-toggle="dropdownRadioBgHover" class="mt-5 btn-primary-indigo inline-flex items-center" type="button">Sort Cars By<svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-                      <div id="dropdownRadioBgHover" ref={dropdownMenuRef}
-                        class={`${isDropdownVisible ? '' : 'hidden'} position-center ml-auto mr-auto mt-1 z-10 w-52 bg-white rounded divide-y divide-gray-100 
+                  <div>
+                    <button id="dropdownRadioBgHoverButton" onClick={toggleDropdown} data-dropdown-toggle="dropdownRadioBgHover" class="mt-5 btn-primary-indigo inline-flex items-center" type="button">Sort Cars By<svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                    <div id="dropdownRadioBgHover" ref={dropdownMenuRef}
+                      class={`${isDropdownVisible ? '' : 'hidden'} position-center ml-auto mr-auto mt-1 z-10 w-52 bg-white rounded divide-y divide-gray-100 
                         shadow dark:bg-gray-700 dark:divide-gray-600`}>
-                        <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioBgHoverButton">
-                          <li>
-                            <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                      <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioBgHoverButton">
+                        <li>
+                          <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
                             <label for="default-radio-1" class="mr-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
                               <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 
                               focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
                               &nbsp; Year - Younger First
-                              </label>
-                            </div>
-                          </li>
-                          <li>
-                            <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                            </label>
+                          </div>
+                        </li>
+                        <li>
+                          <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
                             <label for="default-radio-2" class="text-sm font-medium text-gray-900 rounded dark:text-gray-300">
                               <input id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300
                                focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
                               &nbsp; Year - Older First</label>
-                            </div>
-                          </li>
-                          <li>
-                            <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                          </div>
+                        </li>
+                        <li>
+                          <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
                             <label for="default-radio-3" class="text-sm font-medium text-gray-900 rounded dark:text-gray-300">
                               <input id="default-radio-3" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 
                               focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
                               &nbsp; Price - Lower First</label>
-                            </div>
-                          </li>
-                          <li>
-                            <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                          </div>
+                        </li>
+                        <li>
+                          <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
                             <label for="default-radio-4" class="text-sm font-medium text-gray-900 rounded dark:text-gray-300">
                               <input id="default-radio-4" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 
                               focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
                               &nbsp; Price - Higher First</label>
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
+                          </div>
+                        </li>
+                      </ul>
                     </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -159,9 +159,11 @@ export default function carsForSale({ cars, serverUrl }) {
                 }
               </div>
             </div>
+            {carsList.length === 9 &&
             <div className="w-full text-center">
-                <button className="mt-5 btn-primary-indigo" onClick={getAllCars}>Show more...</button>
-              </div>
+            <button className="mt-5 btn-primary-indigo" onClick={getAllCars}>Show more...</button>
+          </div>
+            }
           </div>
         </section>
 
@@ -175,7 +177,7 @@ export default function carsForSale({ cars, serverUrl }) {
 
 
 export async function getStaticProps({ locale }) {
-  const cars = await loadCars(6);
+  const cars = await loadCars(9);
   const { STATIC_FILES_URL } = process.env;
   return {
     props: {

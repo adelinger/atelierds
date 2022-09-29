bd2acf49f480f35c8b06b3a529077b2cd0e8745a
Antun Delinger <antun994@gmail.com>
2022-09-29 10:44:06 +0200

    fix dropdown

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -66,10 +66,10 @@ export default function carsForSale({ cars, serverUrl }) {
                       t('title_message')
                     }
                   </p>
-                  <div>
+                  <div className="relative">
                     <button id="dropdownRadioBgHoverButton" onClick={toggleDropdown} data-dropdown-toggle="dropdownRadioBgHover" class="mt-5 btn-primary-indigo inline-flex items-center" type="button">Sort Cars By<svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
                     <div id="dropdownRadioBgHover" ref={dropdownMenuRef}
-                      class={`${isDropdownVisible ? '' : 'hidden'} position-center ml-auto mr-auto mt-1 z-10 w-52 bg-white rounded divide-y divide-gray-100 
+                      class={`${isDropdownVisible ? '' : 'hidden'} absolute left-0 right-0 ml-auto mr-auto mt-1 z-10 w-52 bg-white rounded divide-y divide-gray-100 
                         shadow dark:bg-gray-700 dark:divide-gray-600`}>
                       <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioBgHoverButton">
                         <li>

fd2a20f09dfee45fef989050113aa022e5869452
Antun Delinger <antun994@gmail.com>
2022-10-07 08:27:56 +0200

    chrome page edit

--- pages/chrome.js
+++ pages/chrome.js
@@ -43,12 +43,12 @@ export default function chrome() {
     <>
       <Navbar transparent></Navbar>
       <main>
-        <div ref={scrollUpRef} className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+      <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
           <div
             className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
               backgroundImage:
-                "url('/img/light-grey-19.webp')",
+                "url('/img/chrome/chrome_cover.webp')",
             }}
           >
             <span
@@ -57,99 +57,38 @@ export default function chrome() {
             ></span>
           </div>
           <div className="container relative mx-auto">
-            <div className="container mx-auto">
-              <div className="grid grid-cols-1 gap-6 md:grid-cols-2 py-10 md:py-20">
-                <div className="w-full px-4 mr-auto ml-auto -mt-5">
-                  <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto">
-                    <div className="mt-10 text-center">
-                      <h1 className="text-white font-semibold text-3xl">
-                        {t('our_work_title')}
-                      </h1>
-
-                    </div>
-                  </div>
-                  <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-white">
-                    {t('our_work_text_1')}
+            <div className="items-center flex flex-wrap">
+              <div className="w-full lg:w-6/12 mt-5 md:mt-0 md:px-4 ml-auto mr-auto text-center">
+                <div>
+                  <h1 className="text-white font-semibold text-5xl">
+                  {t('our_work_title')}
+                  </h1>
+                  <p className="mt-4 text-lg text-blueGray-200 px-1">
+                  {t('our_work_text_1')}
                   </p>
-                  <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-white">
-                    {t('our_work_text_2')}
-                  </p>
-                  <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-white">
-                    {t('our_work_text_3')}
-                  </p>
-                  <div className='md:mt-10 text-center'>
-                    <button onClick={executeScroll} type="button" class="btn-primary-indigo-slim">
+                  <button onClick={executeScroll} type="button" class="btn-primary-indigo-slim mt-7">
                       {t('see_gallery_btn')}
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-                      </svg>
+                     
                     </button>
-                  </div>
-
-                </div>
-
-                <div className="w-full px-4 mr-auto ml-auto mr-auto">
-                  <div className="relative flex flex-col min-w-0 break-words bg-white w-full md:w-4/6 mb-6 shadow-lg rounded-lg bg-blueGray-700  mx-auto ">
-                    <Image
-                      alt="Citroen ds chrome door part"
-                      src="/img/chrome/ds-door-handle.webp"
-                      className="w-full align-middle rounded-t-lg"
-                      width={500}
-                      height={280}
-                    />
-                    <blockquote className="relative p-8 mb-4">
-                      <svg
-                        preserveAspectRatio="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 583 95"
-                        className="absolute left-0 w-full block h-95-px -top-94-px"
-                      >
-                        <polygon
-                          points="-30,95 583,95 583,65"
-                          className="text-blueGray-700 fill-current"
-                        ></polygon>
-                      </svg>
-                      <h4 className="text-xl font-bold text-white">
-                        {t('quality_service_title')}
-                      </h4>
-                      <p className="text-md font-light mt-2 text-white">
-                        {t('quality_service_text')}
-
-                      </p>
-                    </blockquote>
-                  </div>
                 </div>
               </div>
             </div>
-
-
           </div>
-          <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16"
-            style={{ transform: "translateZ(0)" }}
-          >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-blueGray-700 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
-          </div>
+       
         </div>
+
+        <section className="pb-20 bg-blueGray-200 -mt-24">
+          <div className="container mx-auto px-4">
+
+            
+          </div>
+        </section>
 
         <section className="pb-20 -mb-10 bg-blueGray-700 -mt-24 px-3">
           <div className="container mx-auto px-4 pt-20">
             <div className="container mx-auto mt-5">
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto md:text-center">
-                <div className="md:mt-10">
+                <div className="md:mt-10 pt-5">
                   <h1 className="text-white font-semibold text-3xl">
                     {t('what_do_we_fix_title')}
                   </h1>
@@ -297,6 +236,8 @@ export default function chrome() {
 
           </div>
         </section>
+
+
 
       </main>
       <Footer></Footer>

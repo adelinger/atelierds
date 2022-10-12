42799e7c4d08838f2d0ced8270156e759950c0b3
Antun Delinger <antun994@gmail.com>
2022-10-12 17:27:41 +0200

    hope my UX boss like this pt2

--- pages/leather.js
+++ pages/leather.js
@@ -8,7 +8,7 @@ import ImagePreview from 'components/Modals/ImagePreview';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Image from "next/image";
-import { CircularProgress, LinearProgress } from '@mui/material';
+import { CircularProgress, LinearProgress, Skeleton } from '@mui/material';
 
 
 export default function interior() {
@@ -97,7 +97,7 @@ export default function interior() {
 
         setTimeout(() => {
             setShowLoader(false);
-          }, 500);
+        }, 400);
     }
 
 
@@ -187,7 +187,7 @@ export default function interior() {
                                         onChange={onChangeThumbnail}
                                         autoPlay={useTimer}
                                         infiniteLoop={true}
-                                        onClickItem={executeScroll} 
+                                        onClickItem={executeScroll}
                                         selectedItem={selectedImage}
                                         className='md:cursor-pointer '>
                                         {leather_images_srcs.map((image, index) => {
@@ -254,8 +254,8 @@ export default function interior() {
                         <div className="container mx-auto ">
                             <div className='text-center'>
                                 {showLoader &&
-                                <LinearProgress color="inherit" />
-                                } 
+                                    <LinearProgress color="inherit" />
+                                }
                             </div>
                             <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto md:text-center mb-5">
                                 <div>
@@ -268,15 +268,26 @@ export default function interior() {
                             <div class="container mx-auto space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-4">
                                 {
                                     leather_images[selectedIndex].map((image, index) => {
-                                        return <div class={`${showLoader && 'blur'} w-full rounded md:hover:opacity-50 md:cursor-pointer`}>
-                                            <Image src={image}
-                                                width={500}
-                                                height={400}
-                                                placeholder="blur"
-                                                blurDataURL='/img/blur.png'
-                                                alt="Citroen leather image"
-                                                onClick={() => { toggleModal(), setGalleryImageSelectedIndex(index) }}>
-                                            </Image>
+                                        return <div class={` w-full rounded md:hover:opacity-50 md:cursor-pointer`}>
+                                            {showLoader ?
+                                                <div role="status" class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center">
+                                                    <div class="flex justify-center items-center w-full h-60 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                                                        <svg class="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" /></svg>
+                                                    </div>
+
+                                                    <span class="sr-only">Loading...</span>
+                                                </div>
+                                                :
+                                                <Image src={image}
+                                                    width={500}
+                                                    height={400}
+                                                    placeholder="blur"
+                                                    blurDataURL='/img/blur.png'
+                                                    alt="Citroen leather image"
+                                                    onClick={() => { toggleModal(), setGalleryImageSelectedIndex(index) }}>
+                                                </Image>
+                                            }
+
                                         </div>
                                     })
                                 }

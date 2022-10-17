bdf6e85c5e91e9a01367d85d0a26b3619c581104
Antun Delinger <antun994@gmail.com>
2022-10-17 15:11:36 +0200

    refix validate /again

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -250,7 +250,7 @@ export async function getStaticProps({ locale }) {
       serverUrl: STATIC_FILES_URL,
       ...await serverSideTranslations(locale, ['common', 'carsPage', 'footer']), 
     },
-    revalidate:10,
+    revalidate: 10,
   }
 }
 

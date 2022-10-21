59f22bc863f87689f64ae5c4ebdb1fecbac5e5dd
Antun Delinger <antun994@gmail.com>
2022-10-21 14:37:12 +0200

    remove excess link

--- components/Dropdowns/UserDropdown.js
+++ components/Dropdowns/UserDropdown.js
@@ -46,16 +46,6 @@ const UserDropdown = ({auth}) => {
           "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
         }
       >
-        <a
-          href="#"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Action
-        </a>
-
         <div className="h-0 my-2 border border-solid border-blueGray-100" />
         <a
           href="#"
